9163664b8bdecad16b813f73b7e147ce57b096dc
Nandhinikv <135036226+Nandhinikv@users.noreply.github.com>
2023-06-04 10:37:57 +0400

    arts code

---
+++ Arts.java
@@ -0,0 +1,25 @@
+package org.abstractedu;
+
+public class Arts implements Education {
+
+	@Override
+	public void ug() {
+		System.out.println("bcom");
+	}
+
+	@Override
+	public void pg() {
+		System.out.println("mcom");
+		
+	}
+	private void bsc() {
+		System.out.println("cs");
+
+	}
+public static void main(String[] args) {
+	Arts a= new Arts();
+	a.ug();
+	a.pg();
+	a.bsc();
+}
+}
